bde1b360dff64004cf0c091e671a4c2023851259
maaz <marzusness@gmail.com>
2023-05-09 19:34:46 +0300

    Create error handler middleware that uses ApiError

---
+++ back/util/middleware.ts
@@ -0,0 +1,17 @@
+import { Response, NextFunction, Request } from 'express';
+import { ApiError } from '../classes/ApiError.js';
+import { Error } from '../types.js';
+
+const errorHandler = async (
+  error: ApiError | Error,
+  _req: Request,
+  res: Response<Error>,
+  next: NextFunction
+): Promise<void> => {
+  if (error instanceof ApiError) {
+    res.status(error.status).json({ error: error.message });
+  }
+  next();
+};
+
+export { errorHandler };
