673f56b0ad07f1f32b31f153ee3fa62e6779f43c
maaz <marzusness@gmail.com>
2023-05-09 19:30:50 +0300

    Create class ApiError
    ApiError allows throwing errors with status codes.

---
+++ back/classes/ApiError.ts
@@ -0,0 +1,14 @@
+interface ApiErrorOptions extends ErrorOptions {
+  status?: number;
+}
+
+class ApiError extends Error {
+  status: number;
+
+  constructor(message: string, options?: ApiErrorOptions) {
+    super(message, { cause: options?.cause });
+    this.status = options?.status || 500;
+  }
+}
+
+export { ApiError };
