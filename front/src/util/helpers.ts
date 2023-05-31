bf9c89c680fdeac35def70256a38397bdf22b87e
maaz <marzusness@gmail.com>
2023-05-31 23:25:15 +0300

    Fix createComparator
    This function is used for filtering the stations in the map view. Some
    of the filtering options didn't work correctly and now they are fixed.

--- front/src/util/helpers.ts
+++ front/src/util/helpers.ts
@@ -5,6 +5,55 @@ export const isString = (x: unknown): x is string => typeof x === 'string' || x 
 const isNumber = (x: unknown): x is number =>
   !Number.isNaN(x) && !Number.isNaN(parseFloat(String(x)));
 
+export const createComparator = (
+  filterBy: string | number | null,
+  value: string | string[],
+  operator: Operator
+): boolean => {
+  if (operator === 'isEmpty') {
+    return filterBy === null;
+  }
+
+  if (operator === 'isNotEmpty') {
+    return filterBy !== null;
+  }
+
+  if (Array.isArray(value)) {
+    return value.map((v) => v.toLowerCase()).includes(String(filterBy).toLowerCase());
+  }
+
+  if (isNumber(value) && isNumber(filterBy)) {
+    const valueNum = parseInt(value);
+    return operator === '!='
+      ? filterBy !== valueNum
+      : operator === '<'
+      ? filterBy < valueNum
+      : operator === '<='
+      ? filterBy <= valueNum
+      : operator === '='
+      ? filterBy === valueNum
+      : operator === '>'
+      ? filterBy > valueNum
+      : operator === '>='
+      ? filterBy >= valueNum
+      : false;
+  }
+
+  if (isString(value) && isString(filterBy)) {
+    return operator === 'endsWith'
+      ? value.endsWith(filterBy)
+      : operator === 'equals'
+      ? value === filterBy
+      : operator === 'startsWith'
+      ? value.startsWith(filterBy)
+      : operator === 'contains'
+      ? filterBy.toLowerCase().includes(value.toLowerCase())
+      : false;
+  }
+
+  return false;
+};
+
 export const createTextFile = (text: string): string => {
   const data = new Blob([text], { type: 'text/csv' });
   const csvURL = window.URL.createObjectURL(data);
