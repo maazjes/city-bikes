7b55a7a0da1b6a9600d904f7d20e4e3b4b95fc8e
maaz <marzusness@gmail.com>
2023-06-06 00:28:21 +0300

    Add dots to error messages

--- back/controllers/users.ts
+++ back/controllers/users.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { NODE_ENV } from '../util/config.js';
 import { NewUser } from '../types.js';
 import ApiError from '../classes/ApiError.js';
 import User from '../models/user.js';
@@ -7,14 +8,18 @@ import { hashPassword } from '../util/helpers.js';
 const router = express.Router();
 
 router.post<{}, User, NewUser>('/', async (req, res) => {
+  if (NODE_ENV === 'production') {
+    throw new ApiError('Registration is currently closed!', { status: 400 });
+  }
+
   const { username, password } = req.body;
 
   if (!(username && password)) {
-    throw new ApiError('Email and password are required', { status: 400 });
+    throw new ApiError('Email and password are required.', { status: 400 });
   }
 
   if (password.length < 8) {
-    throw new ApiError('Password must be at least 8 characters long', { status: 400 });
+    throw new ApiError('Password must be at least 8 characters long.', { status: 400 });
   }
 
   const hashedPassword = await hashPassword(password);
