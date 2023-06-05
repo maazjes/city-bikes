0d4200866fd6460bbf502e411a1c1000a47d6864
maaz <marzusness@gmail.com>
2023-06-05 22:52:57 +0300

    Create middleware for authenticating endpoints

--- back/util/middleware.ts
+++ back/util/middleware.ts
@@ -1,6 +1,22 @@
 import { Response, NextFunction, Request } from 'express';
+import jwt from 'jsonwebtoken';
 import ApiError from '../classes/ApiError.js';
 import { Error } from '../types.js';
+import { SECRET } from './config.js';
+
+const requireAuth = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+  const authorization = req.get('authorization');
+  if (!(authorization && authorization.toLowerCase().startsWith('bearer '))) {
+    throw new ApiError('Authentication required', { status: 400 });
+  }
+  const token = authorization.substring(7);
+  try {
+    jwt.verify(token, SECRET);
+  } catch {
+    throw new ApiError('Authentication required', { status: 400 });
+  }
+  return next();
+};
 
 const errorHandler = async (
   error: ApiError | Error,
@@ -14,4 +30,4 @@ const errorHandler = async (
   next();
 };
 
-export { errorHandler };
+export { errorHandler, requireAuth };
