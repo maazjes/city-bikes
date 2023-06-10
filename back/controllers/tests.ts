51b4da1760c91d3531f2db99eaad036982bfdd46
maaz <marzusness@gmail.com>
2023-06-11 01:07:59 +0300

    Make E2E tests create test user for running tests

--- back/controllers/tests.ts
+++ back/controllers/tests.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { Op } from 'sequelize';
-import { Journey, Station } from '../models/index.js';
+import { Journey, Station, User } from '../models/index.js';
 
 const router = express.Router();
 
 router.get('/start', async (_req, res) => {
+  await User.create({ username: 'testtest', password: 'testtest' });
   await Station.create({ id: -1, name: 'test' });
   return res.json({});
 });
@@ -16,6 +17,7 @@ router.get('/end', async (_req, res) => {
     }
   });
   await Station.destroy({ where: { id: { [Op.lt]: 0 } } });
+  await User.destroy({ where: { username: 'testtest' } });
   return res.json({});
 });
 
