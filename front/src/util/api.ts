55fd171e58f8a10599156276b6d3bd8d72a69692
maaz <marzusness@gmail.com>
2023-06-11 01:40:30 +0300

    Change api url based on production or development

--- front/src/util/api.ts
+++ front/src/util/api.ts
@@ -1,4 +1,5 @@
 import { AUTH_TOKEN } from './authToken';
+import { API_URL } from './config';
 
 const setAuthHeaders = (headers?: HeadersInit): HeadersInit | undefined => {
   if (!AUTH_TOKEN) {
@@ -28,7 +29,7 @@ const api = {
     uri: string,
     query?: { [key: string]: string | number | string[] | number[] | Date }
   ): Promise<TResponse> => {
-    let finalQuery = `http://localhost:8080/api/${uri}`;
+    let finalQuery = `${API_URL}${uri}`;
     if (query) {
       finalQuery += '?';
       Object.keys(query).forEach((key) => {
@@ -42,21 +43,21 @@ const api = {
   },
 
   post: <TResponse>(uri: string, body: object): Promise<TResponse> =>
-    request<TResponse>(`http://localhost:8080/api/${uri}`, {
+    request<TResponse>(`${API_URL}${uri}`, {
       method: 'POST',
       headers: setAuthHeaders({ 'Content-Type': 'application/json' }),
       body: JSON.stringify(body)
     }),
 
   postForm: <TResponse>(uri: string, body: FormData): Promise<TResponse> =>
-    request<TResponse>(`http://localhost:8080/api/${uri}`, {
+    request<TResponse>(`${API_URL}${uri}`, {
       method: 'POST',
       headers: setAuthHeaders(),
       body
     }),
 
   delete: <TResponse>(uri: string, body: number[]): Promise<TResponse> =>
-    request<TResponse>(`http://localhost:8080/api/${uri}`, {
+    request<TResponse>(`${API_URL}${uri}`, {
       method: 'DELETE',
       headers: setAuthHeaders({ 'Content-Type': 'application/json' }),
       body: JSON.stringify(body)
--- front/src/util/config.ts
+++ front/src/util/config.ts
@@ -1,3 +1,4 @@
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY!;
+const API_URL = process.env.REACT_APP_API_URL!;
 
-export { GOOGLE_API_KEY };
+export { GOOGLE_API_KEY, API_URL };
